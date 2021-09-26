2cdc0e95f9b0383c5614cc67fdb79d69167f46b9
Henry Baldacci <henry.louis.980@gmail.com>
2021-09-25 21:51:40 -0400

    Minor changes to imports

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import React, { Platform, useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import * as Location from 'expo-location';
 
