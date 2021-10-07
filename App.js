70b5a4fee81e6732e507289c2e4fb5cd0c4d72cc
Henry Baldacci <henry.louis.980@gmail.com>
2021-10-07 11:12:44 -0400

    added login page

--- App.js
+++ App.js
@@ -2,14 +2,16 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from './screens/home';
+import LoginScreen from './screens/login';
 
 const Stack = createNativeStackNavigator();
 
 function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
+      <Stack.Navigator initialRouteName="Login">
         <Stack.Screen name="Home-GPS" component={HomeScreen}/>
+        <Stack.Screen name="Login" component={LoginScreen}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
--- styles/global.js
+++ styles/global.js
@@ -7,4 +7,39 @@ export const globalStyles = StyleSheet.create({
       alignItems: 'center',
       justifyContent: 'center',
     },
+
+    image: {
+      marginBottom: 40,
+    },
+   
+    inputView: {
+      backgroundColor: "#7DF9FF",
+      borderRadius: 30,
+      width: "70%",
+      height: 45,
+      marginBottom: 20,
+      alignItems: "center",
+    },
+   
+    TextInput: {
+      height: 50,
+      flex: 1,
+      padding: 10,
+      marginLeft: 20,
+    },
+   
+    forgot_button: {
+      height: 30,
+      marginBottom: 15,
+    },
+   
+    loginBtn: {
+      width: "80%",
+      borderRadius: 25,
+      height: 50,
+      alignItems: "center",
+      justifyContent: "center",
+      marginTop: 40,
+      backgroundColor: "#0047AB",
+    },
   });
