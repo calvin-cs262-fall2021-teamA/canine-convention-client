a59173432484e11a574dbf10320bec12ca6ca94a
Eric Klomp <ericklomp@Erics-MacBook-Pro.local>
2021-11-12 00:02:34 -0500

    simplified location text

--- screens/home.js
+++ screens/home.js
@@ -1,33 +1,46 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState, useEffect } from 'react';
-import {  Text, View, Image, TouchableOpacity } from 'react-native';
+import {  Text, View, Image, TouchableOpacity, Alert } from 'react-native';
 import * as Location from 'expo-location';
 import { globalStyles } from '../styles/global';
 import logo from '../assets/logo.png'
 
 
 export default function HomeScreen({navigation}) {
-    const [location, setLocation] = useState(null);
-    const [errorMsg, setErrorMsg] = useState(null);
+    const [response, setLocation] = useState(null);
+    //const [errorMsg, setErrorMsg] = useState(null);
   
     useEffect(() => {
-      (async () => {
-        let { status } = await Location.requestForegroundPermissionsAsync();
-        if (status !== 'granted') {
-          setErrorMsg('Permission to access location was denied');
-          return;
-        }
-  
-        let location = await Location.getCurrentPositionAsync({});
-        setLocation(location);
-      })();
+      //CheckIfLocationEnabled();
+      GetCurrentLocation();
     }, []);
-  
-    let locText = 'Waiting..';
-    if (errorMsg) {
-      locText = errorMsg;
-    } else if (location) {
-      locText = JSON.stringify(location);
+
+      const GetCurrentLocation = async () => {
+        let { status } = await Location.requestForegroundPermissionsAsync();
+
+        if (status !== 'granted') {
+          Alert.alert(
+            'Permission not granted', 'Allow the app to use location service.',
+            [{text: 'OK' }],
+            { cancelable: false }
+          );
+        }
+        let {coords} = await Location.getCurrentPositionAsync({});
+        if (coords) {
+          const { latitude, longitude, } = coords;
+          let response = await Location.reverseGeocodeAsync({ latitude, longitude, });
+          setLocation(response);
+          }
+      }
+
+    let addressText = "";
+    if (response){
+      for (let item of response){
+        let name = item.name;
+        let city = item.city;
+        let region = item.region;
+        addressText = JSON.stringify(name + " " + city + ", " + region);
+      }
     }
 
     return (
@@ -49,8 +62,8 @@ export default function HomeScreen({navigation}) {
           <Text style={globalStyles.loginText, globalStyles.ButtonsText}>View Map</Text>
        </TouchableOpacity>
 
-       {/* <Text>{"\n\n"} This is your current location: 
-        {"\n"}{locText}</Text> */}
+        <Text>{"\n\n"} This is your current location: 
+        {"\n"}{addressText}</Text>
        </View> 
     );
   }
