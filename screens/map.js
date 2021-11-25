9ef16bc86b8735fb1f72115eacf29e46b634d633
Henry Baldacci <henry.louis.980@gmail.com>
2021-11-25 16:04:49 -0500

    Bottom sheet added to map

--- screens/map.js
+++ screens/map.js
@@ -1,9 +1,27 @@
-import React, { useEffect, useState } from "react";
+import React, {
+  useEffect,
+  useState,
+  useCallback,
+  useMemo,
+  useRef,
+} from "react";
 import MapView, { PROVIDER_GOOGLE, Marker } from "react-native-maps";
 import * as Location from "expo-location";
-import { BackHandler } from "react-native";
+import { BackHandler, View, Text, StyleSheet } from "react-native";
+import BottomSheet from "@gorhom/bottom-sheet";
 
 export default function Map({ route, navigation }) {
+  // ref
+  const bottomSheetRef = useRef(null);
+
+  // variables
+  const snapPoints = useMemo(() => ["25%", "50%"], []);
+
+  // callbacks
+  const handleSheetChanges = useCallback((index) => {
+    console.log("handleSheetChanges", index);
+  }, []);
+
   const [mapRegion, setMapRegion] = useState(null);
   const [location, setlocation] = useState(null);
   useEffect(() => {
@@ -24,37 +42,59 @@ export default function Map({ route, navigation }) {
     })();
   }, []);
   return (
-    <MapView
-      style={{ flex: 1 }}
-      provider={PROVIDER_GOOGLE}
-      initialRegion={mapRegion}
-      showsUserLocation={true}
-    >
-      <Marker
-        coordinate={{
-          latitude: 42.967310,
-          longitude: -85.639156
-        }}
-        title = "Fuller Park"
-        source={require('../assets/map_marker.png')}>
-        </Marker>
+    <View style={styles.container}>
+      <MapView
+        style={{ flex: 1 }}
+        provider={PROVIDER_GOOGLE}
+        initialRegion={mapRegion}
+        showsUserLocation={true}
+      >
         <Marker
-        coordinate={{
-          latitude: 42.959025,
-          longitude: -85.675905
-        }}
-        title = "Downtown Dog Park"
-        source={require('../assets/map_marker.png')}>
-
-        </Marker>
+          coordinate={{
+            latitude: 42.96731,
+            longitude: -85.639156,
+          }}
+          title="Fuller Park"
+          source={require("../assets/map_marker.png")}
+        ></Marker>
         <Marker
-        coordinate={{
-          latitude: 42.972993,
-          longitude: -85.716083
-        }}
-        title = "Covell"
-        source={require('../assets/map_marker.png')}>
-        </Marker>           
-   </MapView>
+          coordinate={{
+            latitude: 42.959025,
+            longitude: -85.675905,
+          }}
+          title="Downtown Dog Park"
+          source={require("../assets/map_marker.png")}
+        ></Marker>
+        <Marker
+          coordinate={{
+            latitude: 42.972993,
+            longitude: -85.716083,
+          }}
+          title="Covell"
+          source={require("../assets/map_marker.png")}
+        ></Marker>
+      </MapView>
+      <BottomSheet
+        ref={bottomSheetRef}
+        index={1}
+        snapPoints={snapPoints}
+        onChange={handleSheetChanges}
+      >
+        <View style={styles.contentContainer}>
+          <Text>Awesome 🎉</Text>
+        </View>
+      </BottomSheet>
+    </View>
   );
-    }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "grey",
+  },
+  contentContainer: {
+    flex: 1,
+    alignItems: "center",
+  },
+});
