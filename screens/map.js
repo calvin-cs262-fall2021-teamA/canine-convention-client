46a9ed4e20531eab80c9701bf913ac2eb8043806
hbyr99 <henry.louis.980@gmail.com>
2021-11-28 01:20:20 -0500

    Added event finding to bottom sheet

--- screens/map.js
+++ screens/map.js
@@ -7,15 +7,24 @@ import React, {
 } from "react";
 import MapView, { PROVIDER_GOOGLE, Marker } from "react-native-maps";
 import * as Location from "expo-location";
-import { BackHandler, View, Text, StyleSheet } from "react-native";
-import BottomSheet from "@gorhom/bottom-sheet";
+import { BackHandler, View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import BottomSheet, { useBottomSheet } from "@gorhom/bottom-sheet";
+import { ButtonGroup, Icon } from "react-native-elements";
+import { globalStyles } from "../styles/global";
+import SelectDropdown from "react-native-select-dropdown";
+
 
 export default function Map({ route, navigation }) {
+  const [eventType, setEventType] = useState("");
+  const dogNames = ["Fido", "Rover", "Snowball",];
+
   // ref
   const bottomSheetRef = useRef(null);
 
+  const handleExpandPress = () => bottomSheetRef.current.expand()
+
   // variables
-  const snapPoints = useMemo(() => ["25%", "50%"], []);
+  const snapPoints = useMemo(() => ["10%", "30%"], []);
 
   // callbacks
   const handleSheetChanges = useCallback((index) => {
@@ -76,12 +85,58 @@ export default function Map({ route, navigation }) {
       </MapView>
       <BottomSheet
         ref={bottomSheetRef}
-        index={1}
+        index={0}
         snapPoints={snapPoints}
         onChange={handleSheetChanges}
       >
         <View style={styles.contentContainer}>
-          <Text>Awesome 🎉</Text>
+          <SelectDropdown
+            buttonStyle={{
+              backgroundColor: "#FFFFFF",
+              marginBottom: "3%",
+            }}
+            data={dogNames}
+            defaultButtonText="Choose Dog"
+            onSelect={handleExpandPress}
+            buttonTextAfterSelection={(selectedItem, index) => {
+              // text represented after item is selected
+              // if data array is an array of objects then return selectedItem.property to render after item is selected
+              return selectedItem;
+            }}
+            rowTextForSelection={(item, index) => {
+              // text represented for each item in dropdown
+              // if data array is an array of objects then return item.property to represent item in dropdown
+              return item;
+            }}
+            renderDropdownIcon={() => {
+              return (
+                <Icon
+                  name="chevron-down"
+                  type="font-awesome-5"
+                  color={"#444"}
+                  size={18}
+                />
+              );
+            }}
+            dropdownIconPosition={"right"}
+          />
+          <ButtonGroup
+            buttons={["WALK", "PARK"]}
+            selectedButtonStyle={{ backgroundColor: "#16BAC6" }}
+            selectedIndex={eventType}
+            containerStyle={{ marginBottom: "5%" }}
+            onPress={(value) => {
+              setEventType(value);
+            }}
+          />
+          <TouchableOpacity
+            style={globalStyles.homeBtns}
+            onPress={() => navigation.navigate("Match Found", route.params)}
+          >
+            <Text style={(globalStyles.loginText, globalStyles.ButtonsText)}>
+              FIND
+            </Text>
+          </TouchableOpacity>
         </View>
       </BottomSheet>
     </View>
